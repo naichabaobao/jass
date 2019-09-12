4bd97a9555712f2d00f134905731873f4227dd73
家貓hoo <www.395354649.com@qq.com>
2019-09-12 20:08:09 +0800

    '0.0.11'

--- src/resources/angular-viewer/boot.js
+++ src/resources/angular-viewer/boot.js
@@ -1,5 +1,19 @@
 const app = angular.module("boot", []);
 
+app.controller("hello", function ($scope) {
+  $scope.hello = "珍惜反";
+})
+
+app.directive("hello", function () {
+  return {
+    restrict: "E",
+    template: document.querySelector("#home").innerHTML,
+    controller: "hello"
+  }
+})
+
+
+
 app.controller('appController', function ($scope) {
   $scope.hello = "hello angular";
 });
