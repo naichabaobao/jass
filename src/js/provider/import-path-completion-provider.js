691ff5c3c7988ab154888f346a1f52b99e74ab1d
家貓hoo <www.395354649.com@qq.com>
2019-10-02 23:30:14 +0800

    '0.0.19'

--- src/js/provider/import-path-completion-provider.js
+++ src/js/provider/import-path-completion-provider.js
@@ -29,6 +29,54 @@ let inArrayIndex = 0; // 下標中
 
 let inEndFunction = false;
 
+let injector = [];
+
+class Injector {
+  constructor() {
+    this.event.infunction = [];
+    this.event.infunctionname = [];
+    this.event.intakes = [];
+    this.event.intakestype = [];
+    this.event.intakesname = [];
+    this.event.inreturns = [];
+    this.event.inreturnstype = [];
+    this.event.infunctionbody = [];
+    this.event.inlocal = [];
+    this.event.inlocaltype = [];
+    this.event.inlocalname = [];
+    this.event.inlocalarray = [];
+    this.event.inexpression = [];
+    this.event.inexpressionvalue = [];
+    this.event.inexpressionoperator = [];
+    this.event.inpriority = [];
+    this.event.inset = [];
+    this.event.insetname = [];
+    this.event.insetarray = [];
+    this.event.inarrayindex = [];
+    this.event.inendfunction = [];
+  }
+
+  /**
+   * 
+   * @param {string} type 
+   * @param {function} callback 
+   */
+  addEventListener(type, callback) {
+    if (this.event[type] && typeof this.event[type] == 'array' && typeof type == 'string') {
+      this.event[type].push(callback);
+    }
+  }
+
+  /**
+   * 注入字符，
+   * @param {string} char 
+   */
+  push(char) {
+
+  }
+
+}
+
 /**
  * 
  * @param {vscode.TextDocument} document 
