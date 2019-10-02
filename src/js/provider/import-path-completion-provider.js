cbdbedc0b1f526ad40392fc45833878cf57eb557
家貓hoo <www.395354649.com@qq.com>
2019-10-02 22:42:55 +0800

    '0.0.19'

--- src/js/provider/import-path-completion-provider.js
+++ src/js/provider/import-path-completion-provider.js
@@ -1,5 +1,45 @@
 const vscode = require("vscode");
 let createDiagnosticCollection = vscode.languages.createDiagnosticCollection("jass");
+
+let inFunction = false; // 方法中
+let inFunctionName = false;
+let inTakes = false;
+let inTakesType = false;
+let inTakesName = false;
+let inReturns = false;
+let inReturnsType = false;
+let inFunctionBody = false;
+
+let inLocal = false;
+let inLocalType = false;
+let inLocalName = false;
+let inLocalArray = false;
+
+let inExpression = false;
+
+let inExpressionValue = false;
+let inExpressionOperator = false;
+
+let inPriority = 0; // 括號中
+
+let inSet = false;
+let inSetName = false;
+let inSetArray = false;
+let inArrayIndex = 0; // 下標中
+
+let inEndFunction = false;
+
+/**
+ * 
+ * @param {vscode.TextDocument} document 
+ */
+const parse = (document) => {
+  for (let i = 0; i < document.lineCount; i++) {
+    const TextLine = document.lineAt(i);
+  }
+}
+
+
 /**
  * @deprecated 改未错误检测，构思中
  */
